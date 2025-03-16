e12f0326ca79d3ec7ec9e2f5967515eaf7a69475
Maksym Lakhman <maksym.lakhman@gmail.com>
2025-03-16 13:33:48 -0700

    Initial Commit

---
+++ TravelCostCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelCostCalculator
+//
+//  Created by Максим Лахман on 16.03.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelCostCalculator/TravelCostCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCostCalculatorApp.swift
+//  TravelCostCalculator
+//
+//  Created by Максим Лахман on 16.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct TravelCostCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
